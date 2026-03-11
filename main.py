21e13182d7f629d19a90960710bdbaee552279bc
Berta10910 <matteo.bertani@methis.com>
2026-03-11 12:15:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from prices import get_prices
+import requests
 
 app = FastAPI()
 
@@ -21,3 +22,23 @@ def home():
 @app.get("/prices")
 def prices():
     return get_prices()
+
+@app.get("/news")
+def news():
+
+    url = "https://newsapi.org/v2/everything?q=oil OR iran OR opec OR war&language=en&sortBy=publishedAt&pageSize=5&apiKey=YOUR_API_KEY"
+
+    r = requests.get(url)
+
+    data = r.json()
+
+    articles = []
+
+    for a in data["articles"]:
+        articles.append({
+            "title": a["title"],
+            "url": a["url"]
+        })
+
+    return articles
+
